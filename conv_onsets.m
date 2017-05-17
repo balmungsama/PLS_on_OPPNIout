31506b3cafaca7ef766e12e062958bfe869a4658
BalmungSama <jbryaneusebio@gmail.com>
2017-05-17 15:53:45 -0400

    handle the transformation of string to number, and back again

--- conv_onsets.m
+++ conv_onsets.m
@@ -1,10 +1,13 @@
 %%%%% function to transform onsets from MSEC to TRs %%%%%
 
 function YY = conv_onsets(onsets, TR)
+	onsets = str2num(onsets);
 
 	TR = TR * 1000;
 	t_onsets = onsets / TR;
 	t_onsets = floor(t_onsets);
+	t_onsets = num2cell(num2str(t_onsets(:)));
+	t_onsets =  strjoin(t_onsets, ',');
 
 	YY = t_onsets;
 	% return YY
--- read_subjmat.m
+++ read_subjmat.m
@@ -97,8 +97,6 @@ for subj = 1:num_subs
 			t_ons = tline;
 			disp(['t_ons = ', t_ons]);
 			t_ons = conv_onsets(t_ons);
-			t_ons = num2cell(num2str(t_ons(:)));
-			t_ons =  strjoin(t_ons, ',');
 			cond(cond_count).ons   = t_ons;
 		elseif ~isempty(TFdur);
 			cond(cond_count).dur   = tline;
