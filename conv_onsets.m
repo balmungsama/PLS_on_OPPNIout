173b9b7be8d9afd52bfab8205924b00467519520
BalmungSama <jbryaneusebio@gmail.com>
2017-05-18 16:46:14 -0400

    removing a trouble-shooting print statement

--- conv_onsets.m
+++ conv_onsets.m
@@ -2,10 +2,9 @@
 
 function YY = conv_onsets(onsets, TR)
 	onsets = str2num(onsets);
-	disp(onsets);
 
 	TR = TR * 1000;
-	
+
 	t_onsets = onsets / TR;
 	t_onsets = round(t_onsets);  % TODO: decide if you want to use rouond() or floor()
 	t_onsets = sprintf('%.0f,' , t_onsets);
