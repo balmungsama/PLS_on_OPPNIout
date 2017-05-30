055184dcc33b54e9b2d60b94f9b7f71799ba8d31
BalmungSama <jbryaneusebio@gmail.com>
2017-05-30 19:33:51 -0400

    FUCKING WORK

--- write_batch.sh
+++ write_batch.sh
@@ -144,4 +144,4 @@ mREAD_SUBJMAT=$(echo "run('read_subjmat.m')")
 mCOMMANDS=$(echo "$mOS;$mOPPNI_DIR;$mOUTPUT;$mPREFIX;$mBRAIN_ROI;$mWIN_SIZE;$mACROSS_RUN;$mNORM_REF;$mSINGLE_SUBJ;$mREF_ONSET;$mREF_NUM;$mNORMAL;$mRUN;$mREAD_SUBJMAT")
 
 # echo $mCOMMANDS
-$matlab -r "$mCOMMANDS" -nosplash -nodesktop -nosoftwareopengl   #-wait 
+$matlab -r "$mCOMMANDS" -nosplash -nodesktop -nosoftwareopengl #-wait 
