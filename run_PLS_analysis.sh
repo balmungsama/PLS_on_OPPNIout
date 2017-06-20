224f044726e6b66142fd1adc80d23e48e8bc8b3f
BalmungSama <jbryaneusebio@gmail.com>
2017-06-20 15:59:54 -0400

    full file path

--- run_PLS_analysis.sh
+++ run_PLS_analysis.sh
@@ -9,6 +9,6 @@ fi
 
 echo 'Running PLS anlalysis...'
 
-matlab -nodesktop -nosplash -r "pls_file='$FILE';pls_path='$TOP_DIR';run('/home/hpc3586/JE_packages/PLS_on_OPPNIout/run_PLS_anlaysis.m')"
+matlab -nodesktop -nosplash -r "pls_file='$FILE';pls_path='$TOP_DIR';run('/home/hpc3586/JE_packages/PLS_on_OPPNIout/run_PLS_analysis.m')"
 
 echo 'Finished.'
