f11aaf2ae18bed18062761e4b540c15bf5abcfe9
BalmungSama <jbryaneusebio@gmail.com>
2017-07-11 15:17:53 -0400

    fixed location of write_analysis.R call

--- write_analysis.sh
+++ write_analysis.sh
@@ -7,6 +7,8 @@
 #$ -o logs/write_erfmri_analysis.out
 #$ -e logs/write_erfmri_analysis.err
 
+SCRIPT_DIR='/home/hpc3586/JE_packages/PLS_on_OPPNIout'
+
 ##### accept arguments ##### 
 while getopts p:b:v:g:f:r:t:m:c:w:x:y:z:q:s:d:e: option; do
 	case "${option}"
@@ -40,4 +42,4 @@ while getopts p:b:v:g:f:r:t:m:c:w:x:y:z:q:s:d:e: option; do
 	esac
 done
 
-Rscript write_analysis.R --PATH=$PATH --BEHAV_DIR=$BEHAV_DIR --VARBS=$VARBS --GROUPS=$GROUPS --PREFIX=$PREFIX --RM_OUT=$RM_OUT --PLS_opt=$PLS_opt --MEAN_type=$MEAN_type --COR_mode=$COR_mode --num_perm=$num_perm --num_split=$num_split --num_boot=$num_boot --clim=$clim --save_data=$save_data --CONTRASTS==$CONTRASTS --CONDS=$CONDS
+Rscript $SCRIPT_DIR/write_analysis.R --PATH=$PATH --BEHAV_DIR=$BEHAV_DIR --VARBS=$VARBS --GROUPS=$GROUPS --PREFIX=$PREFIX --RM_OUT=$RM_OUT --PLS_opt=$PLS_opt --MEAN_type=$MEAN_type --COR_mode=$COR_mode --num_perm=$num_perm --num_split=$num_split --num_boot=$num_boot --clim=$clim --save_data=$save_data --CONTRASTS==$CONTRASTS --CONDS=$CONDS
