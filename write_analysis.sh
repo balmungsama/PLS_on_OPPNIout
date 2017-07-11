8346c6a9c303e32acfffa9b621e695a0cf19eb56
BalmungSama <jbryaneusebio@gmail.com>
2017-07-10 21:57:18 -0400

    Created wrapper for the write_analysis.R script

--- write_analysis.sh
+++ write_analysis.sh
@@ -11,19 +11,32 @@
 while getopts i:o:p:b:w:a:f:s:r:n:t:z:hc: option; do
 	case "${option}"
 	in
-		i) OPPNI_DIR=${OPTARG};;    # path to the PLS package
-		h) echo "$usage" >&2
-			 exit 1
-			 ;;
-		c) seed=${OPTARG}
-       ;;
-		# :) printf "missing argument for -%s\n" "$OPTARG" >&2
-    #    echo "$usage" >&2
-    #    exit 1
-		# 	 ;;
+		p) PATH=${OPTARG};;
+		b) BEHAV_DIR=${OPTARG};;
+		v) VARBS=${OPTARG};;
+		g) GROUPS=${OPTARG};;
+		f) PREFIX=${OPTARG};;
+		r) RM_OUT=${OPTARG};;
+		t) PLS_opt=${OPTARG};;
+		m) MEAN_type=${OPTARG};;
+		c) COR_mode=${OPTARG};;
+
+		w) num_perm=${OPTARG};;
+		x) num_split=${OPTARG};;
+		y) num_boot=${OPTARG};;
+		z) boot_type=${OPTARG};;
+
+		q) clim=${OPTARG};;
+
+		s) save_data=${OPTARG};;
+
+		d) CONTRASTS=${OPTARG};;
+
 		\?) printf "illegal option: -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
        exit 1
        ;;
 	esac
 done
+
+Rscript write_analysis.R --PATH=$PATH --BEHAV_DIR=$BEHAV_DIR --VARBS=$VARBS --GROUPS=$GROUPS --PREFIX=$PREFIX --RM_OUT=$RM_OUT --PLS_opt=$PLS_opt --MEAN_type=$MEAN_type --COR_mode=$COR_mode --num_perm=$num_perm --num_split=$num_split --num_boot=$num_boot --clim=$clim --save_data=$save_data --CONTRASTS==$CONTRASTS
