2d4cf16b7d477f72a8ddf08f2256c30a2bb7de13
BalmungSama <jbryaneusebio@gmail.com>
2017-07-20 13:32:32 -0400

    removing default values

--- write_batch.sh
+++ write_batch.sh
@@ -17,13 +17,6 @@ MAT_PLUGINS='/home/hpc3586/matlab_plugins'
 INSTALL_DIR='/home/hpc3586/JE_packages/PLS_on_OPPNIout' # enter in the directory in which the package is stored
 cd $INSTALL_DIR                                         # cd to the install directory
 
-##### Default values #####
- 
-WIN_SIZE=8
-NORMAL=0
-RUN=false
-REF_NUM=1
-
 ##### get help Documentation text #####
 
 usage=$(cat write_batch_Documentation.txt)
