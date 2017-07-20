13b0f938aee73b172971051329329dfa435a1757
BalmungSama <jbryaneusebio@gmail.com>
2017-07-20 13:48:02 -0400

    added seperators and bullets to better find different subjects in teh output files

--- run_subjmat.m
+++ run_subjmat.m
@@ -5,7 +5,8 @@ pls_list = dir([PREFIX, '*']);
 pls_list = {pls_list(:).name};
 
 for pls_item = pls_list
-	disp(pls_item{:});
+	disp(' ');
+	disp(['> ', pls_item{:}]);
 	batch_plsgui(pls_item{:});
 end
 
