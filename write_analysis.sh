8a748d643aa3208827297aa15caf8f9a76a2276c
BalmungSama <jbryaneusebio@gmail.com>
2018-02-16 18:27:46 -0500

    got rid or R in environment definition

--- write_analysis.sh
+++ write_analysis.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/usr/bin/R
 #SBATCH -c 4            # Number of CPUS requested. If omitted, the default is 1 CPU.
 #SBATCH --mem=10240     # Memory requested in megabytes. If omitted, the default is 1024 MB.
 #SBATCH -t 0-4:0:0      # How long will your job run for? If omitted, the default is 3 hours.
