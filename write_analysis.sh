9b87b19310bcaf083bcaec63e4e8c95bbab4479f
BalmungSama <jbryaneusebio@gmail.com>
2018-02-16 18:30:04 -0500

    changed variable name

--- write_analysis.sh
+++ write_analysis.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR='/global/home/hpc3586/JE_packages/PLS_on_OPPNIout'
 while getopts p:b:v:g:f:r:t:m:c:w:x:y:z:q:s:d:e:l: option; do
 	case "${option}"
 	in
-		p) PATH=${OPTARG};;
+		p) IN_PATH=${OPTARG};;
 		b) BEHAV_DIR=${OPTARG};;
 		v) VARBS=${OPTARG};;
 		g) GROUPS=${OPTARG};;
@@ -39,4 +39,4 @@ while getopts p:b:v:g:f:r:t:m:c:w:x:y:z:q:s:d:e:l: option; do
 	esac
 done
 
-Rscript $SCRIPT_DIR/write_analysis.R --PATH=$PATH --BEHAV_DIR=$BEHAV_DIR --VARBS=$VARBS --GROUPS=$GROUPS --PREFIX=$PREFIX --RM_OUT=$RM_OUT --PLS_opt=$PLS_opt --MEAN_type=$MEAN_type --COR_mode=$COR_mode --num_perm=$num_perm --num_split=$num_split --num_boot=$num_boot --clim=$clim --save_data=$save_data --CONTRASTS==$CONTRASTS --CONDS=$CONDS --REMOVE_LS=$REMOVE_LS
+Rscript $SCRIPT_DIR/write_analysis.R --PATH=$IN_PATH --BEHAV_DIR=$BEHAV_DIR --VARBS=$VARBS --GROUPS=$GROUPS --PREFIX=$PREFIX --RM_OUT=$RM_OUT --PLS_opt=$PLS_opt --MEAN_type=$MEAN_type --COR_mode=$COR_mode --num_perm=$num_perm --num_split=$num_split --num_boot=$num_boot --clim=$clim --save_data=$save_data --CONTRASTS==$CONTRASTS --CONDS=$CONDS --REMOVE_LS=$REMOVE_LS
