f05e0b21347a02023dffda1c02b1f5031b0bdacd
BalmungSama <jbryaneusebio@gmail.com>
2018-02-16 19:37:43 -0500

    test

--- write_analysis.sh
+++ write_analysis.sh
@@ -9,30 +9,28 @@ SCRIPT_DIR='/global/home/hpc3586/JE_packages/PLS_on_OPPNIout'
 while getopts p:b:v:g:f:r:t:m:c:w:x:y:z:q:s:d:e:l: option; do
 	case "${option}"
 	in
-		p) IN_PATH=${OPTARG};;
-		b) BEHAV_DIR=${OPTARG};;
-		v) VARBS=${OPTARG};;
-		g) GROUPS="${OPTARG}"
-			 echo ${OPTARG}
-			 ;;
-		f) PREFIX=${OPTARG};;
-		r) RM_OUT=${OPTARG};;
-		t) PLS_opt=${OPTARG};;
-		m) MEAN_type=${OPTARG};;
-		c) COR_mode=${OPTARG};;
+		p) IN_PATH=$(echo ${OPTARG});;
+		b) BEHAV_DIR=$(echo ${OPTARG});;
+		v) VARBS=$(echo ${OPTARG});;
+		g) GROUPS=$(echo ${OPTARG});;
+		f) PREFIX=$(echo ${OPTARG});;
+		r) RM_OUT=$(echo ${OPTARG});;
+		t) PLS_opt=$(echo ${OPTARG});;
+		m) MEAN_type=$(echo ${OPTARG});;
+		c) COR_mode=$(echo ${OPTARG});;
 
-		w) num_perm=${OPTARG};;
-		x) num_split=${OPTARG};;
-		y) num_boot=${OPTARG};;
-		z) boot_type=${OPTARG};;
+		w) num_perm=$(echo ${OPTARG});;
+		x) num_split=$(echo ${OPTARG});;
+		y) num_boot=$(echo ${OPTARG});;
+		z) boot_type=$(echo ${OPTARG});;
 
-		q) clim=${OPTARG};;
+		q) clim=$(echo ${OPTARG});;
 
-		s) save_data=${OPTARG};;
+		s) save_data=$(echo ${OPTARG});;
 
-		d) CONTRASTS=${OPTARG};;
-		e) CONDS=${OPTARG};;
-		l) REMOVE_LS=${OPTARG};;
+		d) CONTRASTS=$(echo ${OPTARG});;
+		e) CONDS=$(echo ${OPTARG});;
+		l) REMOVE_LS=$(echo ${OPTARG});;
 
 		\?) printf "illegal option: -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -41,6 +39,24 @@ while getopts p:b:v:g:f:r:t:m:c:w:x:y:z:q:s:d:e:l: option; do
 	esac
 done
 
-echo $GROUPS
+echo $IN_PATH   
+echo $BEHAV_DIR 
+echo $VARBS     
+echo $GROUPS    
+echo $PREFIX    
+echo $RM_OUT    
+echo $PLS_opt   
+echo $MEAN_type 
+echo $COR_mode  
+echo $num_perm  
+echo $num_split 
+echo $num_boot  
+echo $boot_type 
+echo $clim      
+echo $save_data 
+echo $CONTRASTS 
+echo $CONDS     
+echo $REMOVE_LS 
+
 
 Rscript $SCRIPT_DIR/write_analysis.R --PATH=$IN_PATH --BEHAV_DIR=$BEHAV_DIR --VARBS=$VARBS --GROUPS=$GROUPS --PREFIX=$PREFIX --RM_OUT=$RM_OUT --PLS_opt=$PLS_opt --MEAN_type=$MEAN_type --COR_mode=$COR_mode --num_perm=$num_perm --num_split=$num_split --num_boot=$num_boot --clim=$clim --save_data=$save_data --CONTRASTS=$CONTRASTS --CONDS=$CONDS --REMOVE_LS=$REMOVE_LS
