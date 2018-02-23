de96f0099d2084bbf2ad88da083dda386398fdf2
BalmungSama <jbryaneusebio@gmail.com>
2018-02-23 16:47:27 -0500

    removed default wait time

--- run_pls_batch.sh
+++ run_pls_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -c 4            # Number of CPUS requested. If omitted, the default is 1 CPU.
 #SBATCH --mem=10240     # Memory requested in megabytes. If omitted, the default is 1024 MB.
-#SBATCH -t 0-1:0:0      # How long will your job run for? If omitted, the default is 3 hours.
+
 
 echo '	running batch PLS job'
 
