148dc16d8a0c48e63d9b5a437264e1b4a211c31e
BalmungSama <jbryaneusebio@gmail.com>
2018-02-23 16:22:56 -0500

    fixed to run on Frontenac

--- run_PLS_analysis.sh
+++ run_PLS_analysis.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#$ -S /bin/bash
-#$ -cwd
-#$ -M hpc3586@localhost
-#$ -m be
-#$ -q abaqus.q
+#SBATCH -c 4            # Number of CPUS requested. If omitted, the default is 1 CPU.
+#SBATCH --mem=10240     # Memory requested in megabytes. If omitted, the default is 1024 MB.
+#SBATCH -t 0-10:0:0      # How long will your job run for? If omitted, the default is 3 hours.
 
 filename=$1
 
@@ -16,6 +14,6 @@ fi
 
 echo 'Running PLS anlalysis...'
 
-matlab -nodesktop -nosplash -r "pls_file='$FILE';pls_path='$TOP_DIR';run('/home/hpc3586/JE_packages/PLS_on_OPPNIout/run_PLS_analysis.m')"
+matlab -nodesktop -nosplash -r "pls_file='$FILE';pls_path='$TOP_DIR';run('/global/home/hpc3586/JE_packages/PLS_on_OPPNIout/run_PLS_analysis.m')"
 
 echo 'Finished.'
